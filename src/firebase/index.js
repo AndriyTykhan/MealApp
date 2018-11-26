6b824008216396c7de45b6990b37d5bfba1bcfb6
AndriyTykhan <andriytixan@gmail.com>
2018-11-26 22:19:52 +0200

    init

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDJ1WvE2dcQ_NkS_VM4jXwI7AUEKELi4_w',
+  authDomain: 'meal-prep-256cb.firebaseapp.com',
+  databaseURL: 'https://meal-prep-256cb.firebaseio.com',
+  projectId: 'meal-prep-256cb',
+  storageBucket: 'meal-prep-256cb.appspot.com',
+  messagingSenderId: '1007102895417',
+};
+firebase.initializeApp(config);
